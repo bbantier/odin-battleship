56c7fb17b33211a9ecae88256e711fdcf03eaae5
bbantier <65014410+bbantier@users.noreply.github.com>
2025-04-01 15:35:18 +0200

    feat: create gameboard constructor

--- gameboard.js
+++ gameboard.js
@@ -2,8 +2,17 @@ import { Ship } from "./ship.js";
 
 class Gameboard {
   constructor() {
-    this.board = new Array(10).fill(new Array(10));
+    this.ships = [];
+    this.missedShots = new Set();
+    this.attacks = new Set();
+    this.board = Array(10)
+      .fill(null)
+      .map(() => Array(10));
   }
+
+  placeShip(x, y) {}
+
+  receiveAttack() {}
 
   getBoard() {
     return this.board;
