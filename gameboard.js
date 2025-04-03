f3dce30458400c12f1c0dc1184497f0fd29ba844
bbantier <65014410+bbantier@users.noreply.github.com>
2025-04-03 13:36:50 +0200

    feat: create placeShip method

--- gameboard.js
+++ gameboard.js
@@ -2,23 +2,31 @@ import { Ship } from "./ship.js";
 
 class Gameboard {
   constructor() {
-    this.ships = [];
-    this.missedShots = new Set();
-    this.attacks = new Set();
-    this.board = Array(10)
-      .fill(null)
-      .map(() => Array(10));
+    this.board = [];
+    this.fleet = [
+      new Ship("patrol", 2),
+      new Ship("submarine", 3),
+      new Ship("destroyer", 3),
+      new Ship("battleship", 4),
+      new Ship("carrier", 5),
+    ];
   }
 
-  placeShip(x, y) {}
+  placeShip(ship, x, y, orientation = "h") {
+    const shipToPlace = this.fleet.splice(ship, 1)[0];
 
-  receiveAttack() {}
+    for (let i = x; i < shipToPlace.length + x; i++) {
+      if (orientation === "h") {
+        shipToPlace.places.push([x, i]);
+      } else {
+        shipToPlace.places.push([i, y]);
+      }
+    }
 
-  getBoard() {
-    return this.board;
+    this.board.push(shipToPlace);
+  }
+
+  receiveAttack(x, y) {
+
   }
 }
-
-const board = new Gameboard();
-
-console.log(board.getBoard());
--- ship.js
+++ ship.js
@@ -1,14 +1,13 @@
 export class Ship {
-  constructor(length) {
+  constructor(name, length) {
+    this.name = name;
     this.length = length;
     this.hits = 0;
+    this.places = [];
+    this.isSunk = this.hits === this.length;
   }
 
   hit() {
     this.hits++;
-  }
-
-  isSunk() {
-    return this.hits === this.length;
   }
 }
